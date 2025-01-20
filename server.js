ef9e10f5b2b07e27c1933cb71cce6be38fbb592e
Joseph Manning <jgmanning3@yahoo.com>
2025-01-20 16:06:17 -0700

    Update filepath and remove .env file from git cache

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const port = process.env.PORT || 8080;
 const server = require("http").Server(app);
 
 // Serve only the static files form the angularapp directory
-// app.use(express.static(__dirname + "/client/dist/browser/pizza-shop-manager"));
-app.use(express.static("client/dist/browser/pizza-shop-manager"));
+app.use(express.static(__dirname + "/client/dist/browser/pizza-shop-manager"));
 
 app.get("/*", function (req, res) {
   res.sendFile(
-    // path.join(__dirname + "/client/dist/browser/pizza-shop-manager/index.html")
-    path.join("client/dist/browser/pizza-shop-manager/index.html")
+    path.join(__dirname + "/client/dist/browser/pizza-shop-manager/index.html")
   );
 });
 
