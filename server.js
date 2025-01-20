9b139c0cd0c730bc2d396ad3479aef42654e50f4
Joseph Manning <jgmanning3@yahoo.com>
2025-01-20 15:37:36 -0700

    Update file path

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const server = require("http").Server(app);
 
 // Serve only the static files form the angularapp directory
 // app.use(express.static(__dirname + "/client/dist/browser/pizza-shop-manager"));
-app.use(express.static("/client/dist/browser/pizza-shop-manager"));
+app.use(express.static("client/dist/browser/pizza-shop-manager"));
 
 app.get("/*", function (req, res) {
   res.sendFile(
     // path.join(__dirname + "/client/dist/browser/pizza-shop-manager/index.html")
-    path.join("/client/dist/browser/pizza-shop-manager/index.html")
+    path.join("client/dist/browser/pizza-shop-manager/index.html")
   );
 });
 
