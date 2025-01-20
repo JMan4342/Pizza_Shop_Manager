d4766b974080ce851b223658a6db509df80167eb
Joseph Manning <jgmanning3@yahoo.com>
2025-01-20 15:24:28 -0700

    Update file path in server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const server = require('http').Server(app);
 
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist/pizza-shop-manager'));
+app.use(express.static(__dirname + '/client/dist/browser/pizza-shop-manager'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/pizza-shop-manager/index.html'));
+res.sendFile(path.join(__dirname+'/client/dist/browser/pizza-shop-manager/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
