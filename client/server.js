a2288f18d548d5a765f059d000c0498ccf8fba66
Joseph Manning <jgmanning3@yahoo.com>
2025-02-13 15:52:25 -0700

    Removed bodyparser

--- client/server.js
+++ client/server.js
@@ -7,8 +7,8 @@ const path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   express.static(
     path.join(__dirname, "./client/dist/pizza-shop-manager/browser")
