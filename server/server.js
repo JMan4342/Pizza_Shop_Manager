8590d59e207ec3646ff663fb01dce8c86018da1d
Joseph Manning <jgmanning3@yahoo.com>
2025-02-12 17:41:47 -0700

    Add HTML file path back to server.

--- server/server.js
+++ server/server.js
@@ -212,16 +212,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(
-//   express.static(
-//     path.join(__dirname, "../client/dist/pizza-shop-manager/browser")
-//   )
-// );
-// app.get("/*", function (req, res) {
-//   res.sendFile(
-//     path.join(__dirname, "../client/dist/pizza-shop-manager/browser/index.html")
-//   );
-// });
+app.use(
+  express.static(
+    path.join(__dirname, "../client/dist/pizza-shop-manager/browser")
+  )
+);
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/dist/pizza-shop-manager/browser/index.html")
+  );
+});
 
 app.use("/api/pizzas", pizzaRoutes);
 app.use("/api/toppings", toppingRoutes);
