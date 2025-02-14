83cee99cfc4b4480a8cda20bc8fb512634fc2c3a
Joseph Manning <jgmanning3@yahoo.com>
2025-02-14 12:19:07 -0700

    dist browser file only used.

--- server/server.js
+++ server/server.js
@@ -212,11 +212,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(
-//   express.static(
-//     path.join(__dirname, "../client/dist/pizza-shop-manager/browser")
-//   )
-// );
+app.use(
+  express.static(
+    path.join(__dirname, "../client/dist/pizza-shop-manager/browser")
+  )
+);
 // app.get("/*", function (req, res) {
 //   res.sendFile(
 //     path.join(__dirname, "../client/dist/pizza-shop-manager/browser/index.html")
