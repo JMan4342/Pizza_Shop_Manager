3efb621a51b9a0779874563d0853d81ae318ee6f
Joseph Manning <jgmanning3@yahoo.com>
2025-03-04 15:27:20 -0700

    Update build file

--- client/dist/pizza-shop-manager/server/main.server.mjs
+++ client/dist/pizza-shop-manager/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VYLRVYYK.mjs";import"./chunk-XT47TBIN.mjs";import"./chunk-KG5OI3AH.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-SOQFDTPE.mjs";import"./chunk-XT47TBIN.mjs";import"./chunk-KG5OI3AH.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
